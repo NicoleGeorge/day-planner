3945892016d80b0e91015d7f78321167552051af
Nicole George <hello@nicolegeorge.me>
2020-03-21 23:34:18 +1030

    added save button images from front awesome to img folder. added the current date to header in script.js file.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
   
-    console.log(moment());
+    // validation
+
+    var test = false;
+
+    // getting times from moment
+    var currentTime = moment().format("dddd, MMMM Do YYYY");
+
+    // displaying the time in hader
+    var dateHeading = $('#navbar-subtitle');
+    dateHeading.text(currentTime);
+
+// SAVE button - using font awesome icon https://fontawesome.com/license
+  const saveIcon = "./img/save-regular.svg";
+
+  
+// formatting the hous for display
+
+
+
+
+// validation needed - test for after hours 
+    
+if(test) {
+    nowHour24 = 13;
+    nowHour12 = 1;
+}
+ 
+
+// testing stuff as I'm going
+    // console.log(moment().format("dddd, MMMM Do YYYY"));
+
+    // console.log(moment());
   });
