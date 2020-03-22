cf1a954148c11deea6884db15ab70932892cc5d9
Nicole George <hello@nicolegeorge.me>
2020-03-22 22:01:12 +1030

    starting to add the row section one by one. started with the time field.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,72 @@
 $(document).ready(function() {
   
-    // validation
+  // test flag
+  var test = false;
 
-    var test = false;
+  // get times from moment
+  var now = moment().format('MMMM Do YYYY');
 
-    // getting times from moment
-    var currentTime = moment().format("dddd, MMMM Do YYYY");
+  // commented out for test in non-standard hours
+  var nowHour24 = moment().format('H');
+  var nowHour12 = moment().format('h');
 
-    // displaying the time in hader
-    var dateHeading = $('#navbar-subtitle');
-    dateHeading.text(currentTime);
-
-// SAVE button - using font awesome icon https://fontawesome.com/license
-    var saveIcon = "./img/save-regular.svg";
-
-  
-// formatting the hous for display
-
-
-// validation needed - test for after hours 
-    
-if(test) {
+  // set times for tesitng after hours
+  if (test) {
     nowHour24 = 13;
     nowHour12 = 1;
-}
- 
+  }
 
-// testing stuff as I'm going
-    // console.log(moment().format("dddd, MMMM Do YYYY"));
+  var $dateHeading = $('#navbar-subtitle');
+  $dateHeading.text(now);
+  
+  // using font awesome icon https://fontawesome.com/license
 
-    // console.log(moment());
-  });
+  var saveIcon = "./images/save-regular.svg"; 
+
+  // creating the day planner by row for set workplace hours
+
+  for (var hour = 9; hour <= 17; hour++) {
+    // index for array use offset from hour
+    var index = hour - 9;
+    
+    //creating the row components for the calendar
+    // starting with the timeField
+
+    // START: making the time field section
+
+    var $rowDiv = $('<div>');
+    $rowDiv.addClass('row');
+    $rowDiv.addClass('plannerRow');
+    $rowDiv.attr('hour-index', hour);
+  
+    // creating the time field section
+    var $col2TimeDiv = $('<div>');
+    $col2TimeDiv.addClass('col-md-2');
+  
+    // create timeField element
+    var $timeBoxSpn = $('<span>');
+    // can use this to get value
+    $timeBoxSpn.attr('class','timeField');
+    
+    // formatting hours for display
+    var displayHour = 0;
+    var ampm = "";
+    if (hour > 12) { 
+      displayHour = hour - 12;
+      ampm = "pm";
+    } else {
+      displayHour = hour;
+      ampm = "am";
+    }
+    
+    // displaying the timeField section with the workplace set time
+    $timeBoxSpn.text(`${displayHour} ${ampm}`);
+
+    // add the time to col-md-2 - timeField
+    $rowDiv.append($col2TimeDiv);
+    $col2TimeDiv.append($timeBoxSpn);
+    
+    // END: timeField section done
+  }
+    console.log(displayHour);
+});
